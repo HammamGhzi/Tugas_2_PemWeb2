cf10fb106cc18ecd08220ff63809b31c10db405b
HammamGhzi <hammamghazi54@gmail.com>
2026-05-02 20:48:49 +0700

    final

--- src/page/Beranda.tsx
+++ src/page/Beranda.tsx
@@ -1,7 +1,39 @@
 import Button from "../components/ui/Button";
 import Card from "../components/ui/Card";
+import FaqAccordion from "../components/ui/FAQ";
 
 export default function Beranda() {
+  const faqs = [
+    {
+      question: "Apa itu INVOFEST?",
+      answer:
+        "Invofest (Informatics Vocational Festival) adalah festival tahunan yang diakan oleh program studi sarjana terapan teknik informatika Universitas Harkat Negeri, yang bertujuan untuk menginspirasi dan memberdayakan generasi muda Indonesia dalam menghadapi era digital.",
+    },
+    {
+      question: "Kapan dan dimana INVOFEST dilaksanakan?",
+      answer:
+        "INVOFEST diselenggarakan mulai tanggal 21 Oktober 2025 sampai dengan tanggal 27 November 2025.",
+    },
+    {
+      question: "Apakah ada biaya pendaftaran di INVOFEST?",
+      answer: "Semua kegiatan dipastikan berbayar ya teman-teman.",
+    },
+    {
+      question: "Bagaimana saya mengetahui pemenang kompetisi?",
+      answer:
+        "Pemenang akan diinformasikan melalui media sosial instagram dari invofest @invofest_harkatnegeri.",
+    },
+    {
+      question: "Apa yang didapat pemenang dalam kompetisi?",
+      answer:
+        "Pemenang kompetisi akan mendapatkan hadiah trophy, uang pembinaan, dan e-sertifikat.",
+    },
+    {
+      question: "Bagaimana cara mendaftar event?",
+      answer:
+        "Buka website invofest lalu pergi ke halaman event yang anda ingin ikuti dan klik tombol Registrasi.",
+    },
+  ];
   return (
     <div>
       <section
@@ -37,7 +69,6 @@ export default function Beranda() {
           />
         </div>
       </section>
-      
 
       <section className="w-full  mt-20 relative z-10">
         <div className="max-w-7xl ">
@@ -105,6 +136,145 @@ export default function Beranda() {
             </Card>
           </div>
         </div>
+      </section>
+
+      <section
+        id="hero"
+        className="py-10 flex gap-10 justify-between items-center mt-70 "
+      >
+        <div className="w-2/3 flex flex-col gap-6 mr-10">
+          <h1 className="text-red-800 text-5xl font-semibold">IT Seminar</h1>
+
+          <p className=" mr-8 text-sm md:text-base lg:text-[1.35rem]  text-slate-600">
+            Seminar nasional yang membahas strategi dan arsitektur teknologi
+            untuk menciptakan sistem di mana manusia dan AI bekerja sebagai
+            mitra yang sinergis.Yang bertujuan mengubah paradigma dari
+            persaingan menjadi kolaborasi, serta meningkatkan pengetahuan
+            peserta dalam merancang teknologi AI yang berpusat pada manusia.
+          </p>
+
+          <div className="flex gap-3">
+            <Button label="Daftar Sekarang" variant="primary" />
+          </div>
+        </div>
+        <div className="w-1/3">
+          <img
+            src="https://www.invofest-harkatnegeri.com/assets/Maskot-Seminar.png"
+            alt=""
+          />
+        </div>
+      </section>
+
+      <section
+        id="hero talkshow"
+        className="py-10 flex gap-1 justify-between items-center mt-70 "
+      >
+        <div className="w-1/3">
+          <img
+            src="https://www.invofest-harkatnegeri.com/assets/Maskot-Talkshow.png"
+            alt=""
+          />
+        </div>
+        <div className="w-2/3 flex flex-col gap-7  ">
+          <h1 className="text-red-800 text-5xl font-semibold">IT Talkshow</h1>
+
+          <p className=" mr-29 text-sm md:text-base lg:text-[1.35rem]  text-slate-600">
+            Talkshow “Humanizing Technology: Kolaborasi Manusia dan AI di Masa
+            Depan” Sebuah diskusi interaktif yang mengeksplorasi cara
+            mengintegrasikan nilai-nilai kemanusiaan seperti etika, empati, dan
+            kreativitas ke dalam pengembangan kecerdasan buatan. yang bertujuan
+            menginspirasi audiens untuk membangun dan memanfaatkan AI sebagai
+            alat kolaboratif yang memperkuat potensi unik manusia, bukan sebagai
+            penggantinya.
+          </p>
+
+          <div className="flex gap-3">
+            <Button label="Daftar Sekarang" variant="primary" />
+          </div>
+        </div>
+      </section>
+
+      <section
+        id="hero workshop"
+        className="py-10 flex gap-10 justify-between items-center mt-70 "
+      >
+        <div className="w-2/3 flex flex-col gap-6 mr-10">
+          <h1 className="text-red-800 text-5xl font-semibold">IT Workshop</h1>
+
+          <p className=" mr-8 text-sm md:text-base lg:text-[1.35rem]  text-slate-600">
+            Workshop "AI for a Sustainable Future: The Role of Z Generation in
+            the Digital Era” ini menjembatani antara potensi Generasi Z dan
+            kekuatan AI untuk menciptakan masa depan yang berkelanjutan. Peserta
+            akan dibekali wawasan dan alat untuk mentransformasi ide-ide
+            inovatif menjadi solusi lingkungan yang nyata dan terukur di era
+            digital.
+          </p>
+
+          <div className="flex gap-3">
+            <Button label="Daftar Sekarang" variant="primary" />
+          </div>
+        </div>
+        <div className="w-1/3">
+          <img
+            src="https://www.invofest-harkatnegeri.com/assets/Maskot-Workshop.png"
+            alt=""
+          />
+        </div>
+      </section>
+
+      <section
+        id="hero competition"
+        className="py-10 flex gap-10 justify-between items-center "
+      >
+        <div className="w-1/3">
+          <img
+            src="https://www.invofest-harkatnegeri.com/assets/Maskot-Lomba.png"
+            alt=""
+          />
+        </div>
+        <div className="w-2/3 flex flex-col gap-6">
+          <h1 className="text-red-800 text-5xl font-semibold">
+            IT Competition
+          </h1>
+
+          <p className=" mr-8 text-sm md:text-base lg:text-[1.35rem]  text-slate-600">
+            Kompetisi dalam INVOFEST ini mengusung tema “From Creation to
+            Innovation”, Tema ini bertujuan mengajak generasi muda untuk
+            mengembangkan inovasi dan kreativitas guna membentuk kelompok yang
+            memiliki potensi luar biasa, yang mampu mewujudkan masa depan yang
+            berkelanjutan.
+          </p>
+
+          <div className="flex gap-3">
+            <Button label="Info Selengkapnya" variant="primary" />
+            <Button label="Hubungi Panitia" variant="outline" />
+          </div>
+        </div>
+      </section>
+
+      <section id="faq" className="mt-90">
+        <div className="text-center">
+          <div className="mt-4 mb-4">
+            <span className="text-slate-600 text-lg  font-semibold   ">
+              FAQ
+            </span>
+          </div>
+
+          <h2 className="text-slate-600 text-4xl font-bold">
+            Punya Pertanyaan? Lihat{" "}
+          </h2>
+          <h2 className=" text-4xl  text-red-800 font-bold mb-4">Disini</h2>
+          <div className="text-base text-slate-600 mt-5">
+            <p>
+              Ada banyak informasi yang terkait dengan INVOFEST, Anda dapat
+              melihat
+            </p>
+            <p>daftar pertanyaan di bawah ini.</p>
+          </div>
+        </div>
+      </section>
+      <section>
+        <FaqAccordion faqs={faqs} />
       </section>
     </div>
   );
